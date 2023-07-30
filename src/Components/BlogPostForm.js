251e052705973571c5e681537ed46660bc7a4a24
ElizabethD513 <129693714+ElizabethD513@users.noreply.github.com>
2023-07-30 13:44:42 -0400

    kdf

--- src/Components/BlogPostForm.js
+++ src/Components/BlogPostForm.js
@@ -3,17 +3,21 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button'
 
 export default function BlogPostForm() {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // props.createBlog(blogData)
+    }
     return (
         <div>
-            <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Title</Form.Label>
-                    <Form.Control type="text" placeholder="Enter title" />
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="blogTitle">
+                    <Form.Label>Blog Title</Form.Label>
+                    <Form.Control type="text" placeholder="Enter title" name="blogTitle" />
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                     <Form.Label>Example textarea</Form.Label>
-                     <Form.Control as="textarea" rows={9} />
+                <Form.Group className="mb-3" controlId="blogContent">
+                     <Form.Label>Blog Content</Form.Label>
+                     <Form.Control as="textarea" rows={9} name="blogContent"/>
                 </Form.Group>
         
                 <Button variant="primary" type="submit">
--- src/Components/PostList.js
+++ src/Components/PostList.js
@@ -1,7 +1,7 @@
 
 import React, { useEffect, useState} from "react";
 import {Card, Container, Row, Col} from 'react-bootstrap'
-import { getAllBlogs} from '../crud';
+import { getAllBlogs, newBlog} from '../crud';
 
 
 function PostList(props) {
@@ -49,6 +49,23 @@ function PostList(props) {
 	}];
 
     const [blogs, setBlogs] = useState([])
+
+    // function resetForm(){   
+    //     setEditableBlog({
+    //       petName:'',
+    //       petType:'',
+    //       arrivalDate:'',
+    //       id:''
+    //     })
+    //   }
+    
+      function createBlogPost(blogDataFromForm){      //petDataFromForm is being passed to the createPet function
+        newBlog(blogDataFromForm).then(blogDataFromServer => {   //petDataFromForm is being passed to newPet which creates a new pet on the server .then is called when the promise is fulfilled
+         // resetForm()                                       //when the newPet function is successful then the form is reset
+          setBlogs([...blogs, blogDataFromServer])             //updates the pets state with an array containing the new pet data from the server and the existing pets data
+        })
+        
+      }
 
     useEffect(() => {
         getAllBlogs().then(blogs => {
